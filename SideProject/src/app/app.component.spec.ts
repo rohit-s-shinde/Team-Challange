3b10d8a67d5c494bd639ef7fe9e8dc62177ab7bb
Rohit Shinde <rohit.shivaji.shinde@outlook.com>
2020-11-09 21:57:31 +0530

    update project name

--- SideProject/src/app/app.component.spec.ts
+++ SideProject/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'SideProject'`, () => {
+  it(`should have as title 'SignIn'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SideProject');
+    expect(app.title).toEqual('SignIn');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('SideProject app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('SignIn app is running!');
   });
 });
